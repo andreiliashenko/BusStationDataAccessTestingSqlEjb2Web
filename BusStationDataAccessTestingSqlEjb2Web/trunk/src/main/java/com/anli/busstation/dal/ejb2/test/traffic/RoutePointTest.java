bd674e45e967a80548397137af189819241ceec9
lyashenkoav92@gmail.com <lyashenkoav92@gmail.com@a48bbefb-d17f-25e1-05e3-0a94f7aebdbd>
2015-02-23 19:20:40 +0000

    RoutePoint entity integration test implemented

---
+++ BusStationDataAccessTestingSqlEjb2Web/trunk/src/main/java/com/anli/busstation/dal/ejb2/test/traffic/RoutePointTest.java
@@ -0,0 +1,44 @@
+package com.anli.busstation.dal.ejb2.test.traffic;
+
+import com.anli.busstation.dal.ejb2.entities.geography.StationImpl;
+import com.anli.busstation.dal.ejb2.entities.traffic.RoutePointImpl;
+import com.anli.busstation.dal.ejb2.factories.ProxyFactory;
+import com.anli.busstation.dal.ejb2.test.Ejb2FixtureCreator;
+import com.anli.busstation.dal.interfaces.entities.geography.Station;
+import com.anli.busstation.dal.interfaces.entities.traffic.RoutePoint;
+import com.anli.busstation.dal.interfaces.factories.ProviderFactory;
+import com.anli.busstation.dal.test.FixtureCreator;
+import java.math.BigInteger;
+
+public class RoutePointTest extends com.anli.busstation.dal.sql.test.traffic.RoutePointTest {
+
+    @Override
+    protected RoutePoint getNewRoutePoint(BigInteger id, BigInteger stationId, boolean load) {
+        RoutePointImpl routePoint = new RoutePointImpl();
+        routePoint.setId(id);
+        routePoint.setStation(getStationById(stationId, load));
+        return routePoint;
+    }
+
+    @Override
+    protected void nullifyStationCollections(Station station) {
+        StationImpl stationImpl = (StationImpl) station;
+        stationImpl.setBuses(null);
+        stationImpl.setEmployees(null);
+    }
+
+    @Override
+    protected ProviderFactory getFactory() {
+        return new ProxyFactory();
+    }
+
+    @Override
+    protected void setEntityId(RoutePoint entity, BigInteger id) {
+        ((RoutePointImpl) entity).setId(id);
+    }
+
+    @Override
+    protected FixtureCreator getFixtureCreator() {
+        return new Ejb2FixtureCreator();
+    }
+}
