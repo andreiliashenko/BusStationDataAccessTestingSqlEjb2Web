040ba1d2d4c23a54d6ef0ac0282fc81f0c8644af
lyashenkoav92@gmail.com <lyashenkoav92@gmail.com@a48bbefb-d17f-25e1-05e3-0a94f7aebdbd>
2015-03-11 12:56:21 +0000

    1) DataAccess test hierarchy refactored 2) Reference mutation integration test implemented 3) null collection consistency check fixed

---
+++ BusStationDataAccessTestingSqlEjb2Web/trunk/src/main/java/com/anli/busstation/dal/ejb2/test/additional/ReferenceMutationTest.java
@@ -0,0 +1,19 @@
+package com.anli.busstation.dal.ejb2.test.additional;
+
+import com.anli.busstation.dal.ejb2.factories.Ejb2ProviderProxyFactory;
+import com.anli.busstation.dal.ejb2.test.Ejb2FixtureCreator;
+import com.anli.busstation.dal.interfaces.factories.ProviderFactory;
+import com.anli.busstation.dal.test.FixtureCreator;
+
+public class ReferenceMutationTest extends com.anli.busstation.dal.test.additional.ReferenceMutationTest {
+
+    @Override
+    protected FixtureCreator getFixtureCreator() {
+        return new Ejb2FixtureCreator();
+    }
+
+    @Override
+    protected ProviderFactory getFactory() {
+        return new Ejb2ProviderProxyFactory();
+    }
+}
